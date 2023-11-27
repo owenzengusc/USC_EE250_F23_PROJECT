be4dfa84e97a4c3c5d98f99cd76aa4e9498ac017
owenzengusc <owenzeng@usc.edu>
2023-11-26 17:00:52 -0800

    add filter

--- final_project.py
+++ final_project.py
@@ -1,5 +1,7 @@
 import wave, struct     # this will probably also need to be installed
 import pyaudio
+import matplotlib.pyplot as plt
+import numpy as np
 
 RATE = 16000
 FORMAT = pyaudio.paInt16 # 16-bit frames, ie audio is in 2 bytes
@@ -7,19 +9,19 @@ CHANNELS = 1             # mono recording, use 2 if you want stereo
 CHUNK_SIZE = 1024        # bytes
 RECORD_DURATION = 5     # how long the file will be in seconds
 
-# with wave.open("recording.wav", "wb") as wavefile:
-#     p = pyaudio.PyAudio()
-#     wavefile.setnchannels(CHANNELS)
-#     wavefile.setsampwidth(p.get_sample_size(FORMAT))
-#     wavefile.setframerate(RATE)
-#     stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
-#     for _ in range(0, RATE // CHUNK_SIZE * RECORD_DURATION):
-#         wavefile.writeframes(stream.read(CHUNK_SIZE))
-#     stream.close()
+with wave.open("recording.wav", "wb") as wavefile:
+    p = pyaudio.PyAudio()
+    wavefile.setnchannels(CHANNELS)
+    wavefile.setsampwidth(p.get_sample_size(FORMAT))
+    wavefile.setframerate(RATE)
+    stream = p.open(format=FORMAT, channels=CHANNELS, rate=RATE, input=True)
+    for _ in range(0, RATE // CHUNK_SIZE * RECORD_DURATION):
+        wavefile.writeframes(stream.read(CHUNK_SIZE))
+    stream.close()
 
-#     p.terminate()
+    p.terminate()
 
-# print("Done recording")
+print("Done recording")
 # w = wave.open("recording.wav", "rb")
 # for i in range(w.getnframes()):
 #     print(w.readframes(1))
@@ -35,39 +37,78 @@ for i in range(0, length):
     data = struct.unpack("<h", waveData)
     audio_data.append(int(data[0]))
 
-# plot the sound wave
-import matplotlib.pyplot as plt
+plt.figure(figsize=(12, 12)) 
+# Plot the original sound wave
+plt.subplot(2, 2, 1)  # 2 rows, 2 columns, position 1
 plt.plot(audio_data, linestyle="-")
+plt.title("Original Audio Waveform")
+plt.xlabel("Sample")
+plt.ylabel("Amplitude")
+
+
+# Perform FFT
+n = len(audio_data)
+audio_fft = np.fft.fft(audio_data)
+frequencies = np.fft.fftfreq(n, d=1.0/RATE)
+
+# Get the magnitude of the FFT and corresponding frequency values
+magnitude = np.abs(audio_fft)[:n // 2]
+frequencies = frequencies[:n // 2]
+
+# Plot the frequency spectrum
+plt.subplot(2, 2, 2)  # 2 rows, 2 columns, position 2
+plt.plot(frequencies, magnitude)
+plt.title("Original Frequency Spectrum")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude")
+plt.xlim(0, RATE / 2)
+
+
+# Identify the noise frequencies (example: noise between 3000 Hz to 5000 Hz)
+noise_freq_range = (300, 8000)
+
+# Create a filter
+filtered_fft = np.copy(audio_fft)
+for i, freq in enumerate(frequencies):
+    if noise_freq_range[0] <= np.abs(freq) <= noise_freq_range[1]:
+        filtered_fft[i] = 0
+
+# Plot the filtered frequency spectrum
+plt.subplot(2, 2, 4)  # 2 rows, 2 columns, position 4
+plt.plot(frequencies, np.abs(filtered_fft)[:n // 2])
+plt.title("Filtered Frequency Spectrum")
+plt.xlabel("Frequency (Hz)")
+plt.ylabel("Magnitude")
+plt.xlim(0, RATE / 2)
+
+
+# Apply the filter and perform inverse FFT
+filtered_audio = np.fft.ifft(filtered_fft)
+
+
+# Convert the filtered audio back to 16-bit integer format
+# Ensure the real part is taken and properly scaled
+filtered_audio_int = np.int16(filtered_audio.real / np.max(np.abs(filtered_audio.real)) * 32767)
+
+# Plot the filtered sound wave
+plt.subplot(2, 2, 3)  # 2 rows, 2 columns, position 3
+plt.plot(filtered_audio.real, linestyle="-")
+plt.title("Filtered Audio Waveform")
+plt.xlabel("Sample")
+plt.ylabel("Amplitude")
+
+# Display the plots
+plt.tight_layout()  # Adjusts subplot params so that subplots fit into the figure area
 plt.show()
 
-import time
-import grovepi
+# Write the filtered audio to a new wave file
+with wave.open("filtered_recording.wav", "wb") as wavefile:
+    wavefile.setnchannels(CHANNELS)
+    wavefile.setsampwidth(p.get_sample_size(FORMAT))
+    wavefile.setframerate(RATE)
+    for sample in filtered_audio_int:
+        wavefile.writeframes(struct.pack('<h', sample))
 
-# Connect the Grove Buzzer to digital port D8
-# SIG,NC,VCC,GND
-buzzer = 8
-
-grovepi.pinMode(buzzer,"OUTPUT")
-
-while True:
-    try:
-        # # Buzz for 1 second
-        # grovepi.digitalWrite(buzzer,1)
-        # print ('start')
-        # time.sleep(1)
-
-        # # Stop buzzing for 1 second and repeat
-        # grovepi.digitalWrite(buzzer,0)
-        # print ('stop')
-        # time.sleep(1)
-        for i in range (1,255):
-            grovepi.analogWrite(buzzer,i)
-            time.sleep(0.05)
-
-    except KeyboardInterrupt:
-        grovepi.digitalWrite(buzzer,0)
-        break
-    except IOError:
-        print ("Error")
+print("Filtered recording saved as 'filtered_recording.wav'")
 
 
